3c2f2ec4673938249b8036e50c0137b63548ea5b
ImRevyus <77307159+ImRevyus@users.noreply.github.com>
2023-05-16 17:44:09 +0100

    a

--- src/templates/scc.js
+++ src/templates/scc.js
@@ -17,7 +17,7 @@ function ContactPageTemplate({
         <p className="mt-6 text-gray-500 text-lg">
           {contactform.description}
         </p>
-        <p>Thank you for the SUCC</p>
+        <p>Thank you for the SCC</p>
       </TextImageSplit>
 
       <TextImageSplit image={office.image} imageLeft={true}>
