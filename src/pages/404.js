b6b135207dd692b24d69d2b951c71dd59f91bdc6
ImRevyus <77307159+ImRevyus@users.noreply.github.com>
2023-05-17 15:18:10 +0100

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -12,12 +12,12 @@ function PageNotFound() {
         Oops! The page you are looking for has been removed or relocated.
       </p>
       <div>
-        <Link
-          className="mt-6 inline-flex items-center justify-center px-4 py-2 border border-transparent text-xs font-medium transition-colors duration-100 text-btsecondary-accent bg-btsecondary-neutral rounded-md shadow lg:text-sm group hover:bg-btsecondary-hover"
-          to={`/`}
+      <button
+          className="mt-6 inline-flex items-center justify-center px-4 py-2 border border-transparent text-xs font-medium transition-colors duration-100 text-green-600 bg-white rounded-md shadow lg:text-sm group hover:bg-green-50"
+          onClick={() => navigate(-1)}
         >
           Go Back
-        </Link>
+        </button>
       </div>
     </Container>
   )
