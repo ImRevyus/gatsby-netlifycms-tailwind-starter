ad48c672ad1a49d3156b974d48577d770ccc4c2c
ImRevyus <77307159+ImRevyus@users.noreply.github.com>
2023-05-17 23:08:21 +0100

    test

---
+++ src/templates/thankyou.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Header from "../components/Header"
+import { TextImageSplit, SectionHeading } from "../components/Sections"
+
+function ContactPageTemplate({
+  heading,
+  subheading,
+  contactform,
+  office
+}) {
+  return (
+    <div>
+      <Header heading={heading} subheading={subheading} />
+
+      <TextImageSplit image={contactform.image}>
+        <SectionHeading>{contactform.heading}</SectionHeading>
+        <p className="mt-6 text-gray-500 text-lg">
+          {contactform.description}
+        </p>
+        <Container className="text-center h-screen flex flex-col justify-center ">
+      <SectionHeading>Thank you!</SectionHeading>
+      <p className="mt-4">
+        l3az asat!!!
+      </p>
+    </Container>
+      </TextImageSplit>
+
+      <TextImageSplit image={office.image} imageLeft={true}>
+        <p className="text-green-700 font-semibold tracking-wide">
+          {office.tagline}
+        </p>
+        <SectionHeading>{office.location}</SectionHeading>
+        <div className="mt-6 sm:flex sm:flex-row text-gray-500">
+          <div
+            className="sm:w-1/2"
+            dangerouslySetInnerHTML={{ __html: office.address.html }}
+          />
+          <div
+            className="mt-6 sm:mt-0 sm:w-1/2"
+            dangerouslySetInnerHTML={{ __html: office.phone.html }}
+          />
+        </div>
+      </TextImageSplit>
+    </div>
+  )
+}
+
+export default ContactPageTemplate
