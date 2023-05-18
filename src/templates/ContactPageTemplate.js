3af3da9b78fd59141c937c19226227f5425a6734
ImRevyus <77307159+ImRevyus@users.noreply.github.com>
2023-05-18 21:50:10 +0100

    sdsds

--- src/templates/ContactPageTemplate.js
+++ src/templates/ContactPageTemplate.js
@@ -35,7 +35,7 @@ function ContactPageTemplate({
         <form
           name="contact"
           method="post"
-          action="/thankyou/"
+          action="\pages\thankyou"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
           onSubmit={handleSubmit}
